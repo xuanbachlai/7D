d194105599b3911605e02b0672cbe4b99d211794
xuanbachlai <142763274+xuanbachlai@users.noreply.github.com>
2023-10-31 16:50:31 +0700

    Add files via upload

--- js/klmn.js
+++ js/klmn.js
@@ -1,2 +1,4 @@
 tag0 = document.getElementById('tag0')
 tag0.onclick = function changeIT(){document.location="knowledage/ITTermI.html"}
+tag1 = document.getElementById('tag1')
+tag1.onclick = function changeGG(){document.location="knowledage/GGTermI.html"}
